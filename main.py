abc6fbe0685eac957e17d53e3c335518b0eb3f00
lwz <905328134@qq.com>
2024-08-23 22:40:27 +0800

    initial

---
+++ main.py
@@ -0,0 +1,315 @@
+import time
+
+import torch.utils.data
+import torchvision
+import torchvision.transforms
+from torch_official_code import utils, presets
+from torch_official_code.sampler import RASampler
+from torch.utils.data.dataloader import default_collate
+from torchvision.transforms.functional import InterpolationMode
+from torch_official_code.transforms import get_mixup_cutmix
+
+import torch
+import lightning as pl
+from lightning.pytorch import cli
+from lightning.pytorch.callbacks.model_checkpoint import ModelCheckpoint
+from lightning.pytorch.callbacks.lr_monitor import LearningRateMonitor
+from lightning.pytorch.loggers import WandbLogger
+from model.vision_classifier import VisionClassifier
+import os
+
+os.environ['CURL_CA_BUNDLE'] = ''
+
+
+def _get_cache_path(filepath):
+    import hashlib
+
+    h = hashlib.sha1(filepath.encode()).hexdigest()
+    cache_path = os.path.join("~", ".torch", "vision", "datasets", "imagefolder", h[:10] + ".pt")
+    cache_path = os.path.expanduser(cache_path)
+    return cache_path
+
+
+def load_data(traindir, valdir, args):
+    # Data loading code
+    print("Loading data")
+    val_resize_size, val_crop_size, train_crop_size = (
+        args.val_resize_size,
+        args.val_crop_size,
+        args.train_crop_size,
+    )
+    interpolation = InterpolationMode(args.interpolation)
+
+    print("Loading training data")
+    st = time.time()
+    cache_path = _get_cache_path(traindir)
+    if args.cache_dataset and os.path.exists(cache_path):
+        # Attention, as the transforms are also cached!
+        print(f"Loading dataset_train from {cache_path}")
+        # TODO: this could probably be weights_only=True
+        dataset, _ = torch.load(cache_path, weights_only=False)
+    else:
+        # We need a default value for the variables below because args may come
+        # from train_quantization.py which doesn't define them.
+        auto_augment_policy = getattr(args, "auto_augment", None)
+        random_erase_prob = getattr(args, "random_erase", 0.0)
+        ra_magnitude = getattr(args, "ra_magnitude", None)
+        augmix_severity = getattr(args, "augmix_severity", None)
+        dataset = torchvision.datasets.ImageFolder(
+            traindir,
+            presets.ClassificationPresetTrain(
+                crop_size=train_crop_size,
+                interpolation=interpolation,
+                auto_augment_policy=auto_augment_policy,
+                random_erase_prob=random_erase_prob,
+                ra_magnitude=ra_magnitude,
+                augmix_severity=augmix_severity,
+                backend=args.backend,
+                use_v2=args.use_v2,
+            ),
+        )
+        if args.cache_dataset:
+            print(f"Saving dataset_train to {cache_path}")
+            utils.mkdir(os.path.dirname(cache_path))
+            utils.save_on_master((dataset, traindir), cache_path)
+    print("Took", time.time() - st)
+
+    print("Loading validation data")
+    cache_path = _get_cache_path(valdir)
+    if args.cache_dataset and os.path.exists(cache_path):
+        # Attention, as the transforms are also cached!
+        print(f"Loading dataset_test from {cache_path}")
+        # TODO: this could probably be weights_only=True
+        dataset_test, _ = torch.load(cache_path, weights_only=False)
+    else:
+        if args.weights and args.test_only:
+            weights = torchvision.models.get_weight(args.weights)
+            preprocessing = weights.transforms(antialias=True)
+            if args.backend == "tensor":
+                preprocessing = torchvision.transforms.Compose([torchvision.transforms.PILToTensor(), preprocessing])
+
+        else:
+            preprocessing = presets.ClassificationPresetEval(
+                crop_size=val_crop_size,
+                resize_size=val_resize_size,
+                interpolation=interpolation,
+                backend=args.backend,
+                use_v2=args.use_v2,
+            )
+
+        dataset_test = torchvision.datasets.ImageFolder(
+            valdir,
+            preprocessing,
+        )
+        if args.cache_dataset:
+            print(f"Saving dataset_test to {cache_path}")
+            utils.mkdir(os.path.dirname(cache_path))
+            utils.save_on_master((dataset_test, valdir), cache_path)
+
+    print("Creating data loaders")
+    if args.distributed:
+        if hasattr(args, "ra_sampler") and args.ra_sampler:
+            train_sampler = RASampler(dataset, shuffle=True, repetitions=args.ra_reps)
+        else:
+            train_sampler = torch.utils.data.distributed.DistributedSampler(dataset)
+        test_sampler = torch.utils.data.distributed.DistributedSampler(dataset_test, shuffle=False)
+    else:
+        train_sampler = torch.utils.data.RandomSampler(dataset)
+        test_sampler = torch.utils.data.SequentialSampler(dataset_test)
+
+    return dataset, dataset_test, train_sampler, test_sampler
+
+
+def main(args):
+    train_dir = os.path.join(args.data_path, "train")
+    val_dir = os.path.join(args.data_path, "val")
+    dataset, dataset_test, train_sampler, test_sampler = load_data(train_dir, val_dir, args)
+    num_classes = len(dataset.classes)
+    args.num_classes = num_classes
+    mixup_cutmix = get_mixup_cutmix(
+        mixup_alpha=args.mixup_alpha, cutmix_alpha=args.cutmix_alpha, num_classes=num_classes, use_v2=args.use_v2
+    )
+    if mixup_cutmix is not None:
+        def collate_fn(batch):
+            return mixup_cutmix(*default_collate(batch))
+
+    else:
+        collate_fn = default_collate
+
+    data_loader = torch.utils.data.DataLoader(
+        dataset,
+        batch_size=args.batch_size,
+        sampler=train_sampler,
+        num_workers=args.workers,
+        pin_memory=True,
+        collate_fn=collate_fn,
+    )
+
+    data_loader_test = torch.utils.data.DataLoader(
+        dataset_test, batch_size=args.batch_size, sampler=test_sampler, num_workers=args.workers, pin_memory=True
+    )
+
+    checkpoint_callback = ModelCheckpoint(**args.model_checkpoint)
+    lr_monitor = LearningRateMonitor(**args.lr_monitor)
+    wandb_logger = WandbLogger(
+        name=args.name,
+        project=args.project,
+        offline=args.offline,
+        log_model=False
+    )
+    trainer = pl.Trainer(
+        **args.trainer,
+        logger=wandb_logger,
+        callbacks=[checkpoint_callback, lr_monitor],
+
+    )
+    model = VisionClassifier(args)
+    trainer.fit(model, train_dataloaders=data_loader, val_dataloaders=data_loader_test)
+
+
+def get_args_parser(add_help=True):
+    # import argparse
+    # parser = argparse.ArgumentParser(description="PyTorch Classification Training", add_help=add_help)
+
+    parser = cli.LightningArgumentParser()
+    parser.add_lightning_class_args(pl.Trainer, 'trainer')
+    parser.add_lightning_class_args(ModelCheckpoint, "model_checkpoint")
+    parser.add_lightning_class_args(LearningRateMonitor, "lr_monitor")
+
+    parser.add_argument("--data-path", default="/datasets01/imagenet_full_size/061417/", type=str, help="dataset path")
+    parser.add_argument("--model", default="resnet18", type=str, help="model name")
+    parser.add_argument("--device", default="cuda", type=str, help="device (Use cuda or cpu Default: cuda)")
+    parser.add_argument(
+        "-b", "--batch-size", default=32, type=int, help="images per gpu, the total batch size is $NGPU x batch_size"
+    )
+    parser.add_argument("--epochs", default=90, type=int, metavar="N", help="number of total epochs to run")
+    parser.add_argument(
+        "-j", "--workers", default=16, type=int, metavar="N", help="number of data loading workers (default: 16)"
+    )
+    parser.add_argument("--opt", default="sgd", type=str, help="optimizer")
+    parser.add_argument("--lr", default=0.1, type=float, help="initial learning rate")
+    parser.add_argument("--momentum", default=0.9, type=float, metavar="M", help="momentum")
+    parser.add_argument(
+        "--wd",
+        "--weight-decay",
+        default=1e-4,
+        type=float,
+        metavar="W",
+        help="weight decay (default: 1e-4)",
+        dest="weight_decay",
+    )
+    parser.add_argument(
+        "--norm-weight-decay",
+        default=None,
+        type=float,
+        help="weight decay for Normalization layers (default: None, same value as --wd)",
+    )
+    parser.add_argument(
+        "--bias-weight-decay",
+        default=None,
+        type=float,
+        help="weight decay for bias parameters of all layers (default: None, same value as --wd)",
+    )
+    parser.add_argument(
+        "--transformer-embedding-decay",
+        default=None,
+        type=float,
+        help="weight decay for embedding parameters for vision transformer models (default: None, same value as --wd)",
+    )
+    parser.add_argument(
+        "--label-smoothing", default=0.0, type=float, help="label smoothing (default: 0.0)", dest="label_smoothing"
+    )
+    parser.add_argument("--mixup-alpha", default=0.0, type=float, help="mixup alpha (default: 0.0)")
+    parser.add_argument("--cutmix-alpha", default=0.0, type=float, help="cutmix alpha (default: 0.0)")
+    parser.add_argument("--lr-scheduler", default="steplr", type=str, help="the lr scheduler (default: steplr)")
+    parser.add_argument("--lr-warmup-epochs", default=0, type=int, help="the number of epochs to warmup (default: 0)")
+    parser.add_argument(
+        "--lr-warmup-method", default="constant", type=str, help="the warmup method (default: constant)"
+    )
+    parser.add_argument("--lr-warmup-decay", default=0.01, type=float, help="the decay for lr")
+    parser.add_argument("--lr-step-size", default=30, type=int, help="decrease lr every step-size epochs")
+    parser.add_argument("--lr-gamma", default=0.1, type=float, help="decrease lr by a factor of lr-gamma")
+    parser.add_argument("--lr-min", default=0.0, type=float, help="minimum lr of lr schedule (default: 0.0)")
+    parser.add_argument("--print-freq", default=10, type=int, help="print frequency")
+    parser.add_argument("--output-dir", default=".", type=str, help="path to save outputs")
+    parser.add_argument("--resume", default="", type=str, help="path of checkpoint")
+    parser.add_argument("--start-epoch", default=0, type=int, metavar="N", help="start epoch")
+    parser.add_argument(
+        "--cache-dataset",
+        dest="cache_dataset",
+        help="Cache the datasets for quicker initialization. It also serializes the transforms",
+        action="store_true",
+    )
+    parser.add_argument(
+        "--sync-bn",
+        dest="sync_bn",
+        help="Use sync batch norm",
+        action="store_true",
+    )
+    parser.add_argument(
+        "--test-only",
+        dest="test_only",
+        help="Only test the model",
+        action="store_true",
+    )
+    parser.add_argument("--auto-augment", default=None, type=str, help="auto augment policy (default: None)")
+    parser.add_argument("--ra-magnitude", default=9, type=int, help="magnitude of auto augment policy")
+    parser.add_argument("--augmix-severity", default=3, type=int, help="severity of augmix policy")
+    parser.add_argument("--random-erase", default=0.0, type=float, help="random erasing probability (default: 0.0)")
+
+    # Mixed precision training parameters
+    parser.add_argument("--amp", action="store_true", help="Use torch.cuda.amp for mixed precision training")
+
+    # distributed training parameters
+    parser.add_argument("--world-size", default=1, type=int, help="number of distributed processes")
+    parser.add_argument("--dist-url", default="env://", type=str, help="url used to set up distributed training")
+    parser.add_argument(
+        "--model-ema", action="store_true", help="enable tracking Exponential Moving Average of model parameters"
+    )
+    parser.add_argument(
+        "--model-ema-steps",
+        type=int,
+        default=32,
+        help="the number of iterations that controls how often to update the EMA model (default: 32)",
+    )
+    parser.add_argument(
+        "--model-ema-decay",
+        type=float,
+        default=0.99998,
+        help="decay factor for Exponential Moving Average of model parameters (default: 0.99998)",
+    )
+    parser.add_argument(
+        "--use-deterministic-algorithms", action="store_true", help="Forces the use of deterministic algorithms only."
+    )
+    parser.add_argument(
+        "--interpolation", default="bilinear", type=str, help="the interpolation method (default: bilinear)"
+    )
+    parser.add_argument(
+        "--val-resize-size", default=256, type=int, help="the resize size used for validation (default: 256)"
+    )
+    parser.add_argument(
+        "--val-crop-size", default=224, type=int, help="the central crop size used for validation (default: 224)"
+    )
+    parser.add_argument(
+        "--train-crop-size", default=224, type=int, help="the random crop size used for training (default: 224)"
+    )
+    parser.add_argument("--clip-grad-norm", default=None, type=float, help="the maximum gradient norm (default None)")
+    parser.add_argument("--ra-sampler", action="store_true", help="whether to use Repeated Augmentation in training")
+    parser.add_argument(
+        "--ra-reps", default=3, type=int, help="number of repetitions for Repeated Augmentation (default: 3)"
+    )
+    parser.add_argument("--weights", default=None, type=str, help="the weights enum name to load")
+    parser.add_argument("--backend", default="PIL", type=str.lower, help="PIL or tensor - case insensitive")
+    parser.add_argument("--use-v2", action="store_true", help="Use V2 transforms")
+
+    # wandb
+    parser.add_argument('--name', default='debug')
+    parser.add_argument('--project', default='debug')
+    parser.add_argument('--offline', action='store_true', default=False)
+
+    return parser
+
+
+if __name__ == "__main__":
+    args = get_args_parser().parse_args()
+    main(args)
---
+++ model/vision_classifier.py
@@ -0,0 +1,154 @@
+import torch
+from timm.utils import accuracy
+from lightning import LightningModule
+from torch_official_code import utils
+import torchvision
+
+
+class VisionClassifier(LightningModule):
+    def __init__(
+            self,
+            config,
+            *args,
+            **kwargs,
+    ) -> None:
+        super().__init__(*args, **kwargs)
+        self.config = config
+        self.save_hyperparameters()
+
+        # print(f"Creating model: {config.model}")
+        # self.model = create_model(
+        #     model_name=config.model,
+        #     pretrained=False,
+        #     num_classes=config.nb_classes,
+        #     drop_rate=config.drop,
+        #     drop_path_rate=config.drop_path,
+        #     drop_block_rate=None,
+        #     img_size=config.input_size
+        # )
+
+        print(f"Creating model: {config.model}")
+        self.model = torchvision.models.get_model(
+            name=config.model,
+            weights=config.weights,
+            num_classes=config.num_classes
+        )
+
+        # model EMA
+        model_ema = None
+        if config.model_ema:
+            # Decay adjustment that aims to keep the decay independent of other hyper-parameters originally proposed at:
+            # https://github.com/facebookresearch/pycls/blob/f8cd9627/pycls/core/net.py#L123
+            #
+            # total_ema_updates = (Dataset_size / n_GPUs) * epochs / (batch_size_per_gpu * EMA_steps)
+            # We consider constant = Dataset_size for a given dataset/setup and omit it. Thus:
+            # adjust = 1 / total_ema_updates ~= n_GPUs * batch_size_per_gpu * EMA_steps / epochs
+            adjust = config.world_size * config.batch_size * config.model_ema_steps / config.epochs
+            alpha = 1.0 - config.model_ema_decay
+            alpha = min(1.0, alpha * adjust)
+            model_ema = utils.ExponentialMovingAverage(self.model, device=self.device, decay=1.0 - alpha)
+        self.model_ema = model_ema
+
+    def training_step(self, batch, batch_idx):
+        image, target = batch
+        output = self.model(image)
+        loss = self.criterion(output, target)
+        self.log('train/loss', loss, sync_dist=True)
+
+        return loss
+
+    def validation_step(self, batch, batch_idx):
+        image, target = batch
+        output = self.model(image)
+        loss = self.criterion(output, target)
+        acc1, acc5 = accuracy(output, target, topk=(1, 5))
+
+        self.log('val/loss', loss, sync_dist=True)
+        self.log('val/acc1', acc1, sync_dist=True)
+        self.log('val/acc5', acc5, sync_dist=True)
+
+        return loss
+
+    def configure_optimizers(self):
+        config = self.config
+        custom_keys_weight_decay = []
+        if config.bias_weight_decay is not None:
+            custom_keys_weight_decay.append(("bias", config.bias_weight_decay))
+        if config.transformer_embedding_decay is not None:
+            for key in ["class_token", "position_embedding", "relative_position_bias_table"]:
+                custom_keys_weight_decay.append((key, config.transformer_embedding_decay))
+        parameters = utils.set_weight_decay(
+            self.model,
+            config.weight_decay,
+            norm_weight_decay=config.norm_weight_decay,
+            custom_keys_weight_decay=custom_keys_weight_decay if len(custom_keys_weight_decay) > 0 else None,
+        )
+
+        opt_name = config.opt.lower()
+        if opt_name.startswith("sgd"):
+            optimizer = torch.optim.SGD(
+                parameters,
+                lr=config.lr,
+                momentum=config.momentum,
+                weight_decay=config.weight_decay,
+                nesterov="nesterov" in opt_name,
+            )
+        elif opt_name == "rmsprop":
+            optimizer = torch.optim.RMSprop(
+                parameters, lr=config.lr, momentum=config.momentum, weight_decay=config.weight_decay, eps=0.0316,
+                alpha=0.9
+            )
+        elif opt_name == "adamw":
+            optimizer = torch.optim.AdamW(parameters, lr=config.lr, weight_decay=config.weight_decay)
+        else:
+            raise RuntimeError(f"Invalid optimizer {config.opt}. Only SGD, RMSprop and AdamW are supported.")
+
+        config.lr_scheduler = config.lr_scheduler.lower()
+        if config.lr_scheduler == "steplr":
+            main_lr_scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=config.lr_step_size,
+                                                                gamma=config.lr_gamma)
+        elif config.lr_scheduler == "cosineannealinglr":
+            main_lr_scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(
+                optimizer, T_max=config.epochs - config.lr_warmup_epochs, eta_min=config.lr_min
+            )
+        elif config.lr_scheduler == "exponentiallr":
+            main_lr_scheduler = torch.optim.lr_scheduler.ExponentialLR(optimizer, gamma=config.lr_gamma)
+        else:
+            raise RuntimeError(
+                f"Invalid lr scheduler '{config.lr_scheduler}'. Only StepLR, CosineAnnealingLR and ExponentialLR "
+                "are supported."
+            )
+
+        if config.lr_warmup_epochs > 0:
+            if config.lr_warmup_method == "linear":
+                warmup_lr_scheduler = torch.optim.lr_scheduler.LinearLR(
+                    optimizer, start_factor=config.lr_warmup_decay, total_iters=config.lr_warmup_epochs
+                )
+            elif config.lr_warmup_method == "constant":
+                warmup_lr_scheduler = torch.optim.lr_scheduler.ConstantLR(
+                    optimizer, factor=config.lr_warmup_decay, total_iters=config.lr_warmup_epochs
+                )
+            else:
+                raise RuntimeError(
+                    f"Invalid warmup lr method '{config.lr_warmup_method}'. Only linear and constant are supported."
+                )
+            lr_scheduler = torch.optim.lr_scheduler.SequentialLR(
+                optimizer, schedulers=[warmup_lr_scheduler, main_lr_scheduler], milestones=[config.lr_warmup_epochs]
+            )
+        else:
+            lr_scheduler = main_lr_scheduler
+
+        return {
+            "optimizer": optimizer,
+            "lr_scheduler": lr_scheduler,
+        }
+
+    def lr_scheduler_step(self, scheduler, metric):
+        scheduler.step(epoch=self.current_epoch)  # timm's scheduler need the epoch value
+
+    def on_before_backward(self, loss: torch.Tensor) -> None:
+        if self.model_ema:
+            self.model_ema.update_parameters(self.model)
+            if self.current_epoch < self.config.lr_warmup_epochs:
+                # Reset ema buffer to keep copying weights during warmup period
+                self.model_ema.n_averaged.fill_(0)
